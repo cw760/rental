ad1c5ab11385c2470d33499959534486a39c27a4
cw760 <cw760@163.com>
2014-05-05 15:08:20 +0800

    add login() for android
    Change-Id: I9881771f38b3eaed7364deb4e953bb97ac51bce4

--- rental-server/src/main/java/org/incito/cw/rental/service/UserService.java
+++ rental-server/src/main/java/org/incito/cw/rental/service/UserService.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.incito.cw.rental.controller.UserController;
 import org.incito.cw.rental.dao.UserDao;
 import org.incito.cw.rental.model.User;
+import org.incito.cw.rental.utils.Encrypt;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -27,6 +30,24 @@ public class UserService {
   public List<User> list() {
     logger.debug("get user list");
     return userDao.getUserList();
+  }
+
+  @RequestMapping(method = RequestMethod.POST, value = "login")
+  @ResponseBody
+  public User valid(@RequestBody User user) throws Exception {
+    logger.debug("login " + user);
+    if (StringUtils.isEmpty(user.getAccount())) {
+      throw new Exception("账户不能为空");
+    }
+    if (StringUtils.isEmpty(user.getPwd())) {
+      throw new Exception("密码不能为空");
+    }
+    User findByAccount = userDao.findByAccount(user.getAccount());
+    if (Encrypt.encrypt(user.getPwd()).equals(findByAccount.getPwd())) {
+      return findByAccount;
+    } else {
+      throw new Exception("用户名密码不正确");
+    }
   }
 
 }
